1c5e7324ef5b3c23bfcd7d7c4bd556d266007a60
Operator-One <cullen.guimond@guinet.us>
2024-03-28 11:14:49 -0400

    Debugging

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ec2-manager',
+    name='Ec2Manager',
     version='0.1.0',
     author='Operator-One',
     author_email='cullen.guimond@guinet.us',
     description='Allows you to manage and create resources in AWS EC2',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/Operator-One/ec2-manager',
+    url='https://github.com/Operator-One/Ec2Manager',
     packages=find_packages(),
     install_requires=[
         'boto3>=1.17',
         'inquirer>=2.7.0',
     ],
-    python_requires='>=3.10',
-    entry_points={
-        'console_scripts': [
-            'ec2-manager=ec2-manager.main:main',
-        ],
+entry_points={
+    'console_scripts': [
+        'ec2-manager=ec2_manager.main:main',
+    ],
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
